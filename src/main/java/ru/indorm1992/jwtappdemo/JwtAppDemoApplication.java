1600666f3dad1be04f3d891aed7264b6830f316b
tral909 <indorm1992@mail.ru>
2020-07-07 18:46:07 +0300

    init project

---
+++ src/main/java/ru/indorm1992/jwtappdemo/JwtAppDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.indorm1992.jwtappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAppDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/indorm1992/jwtappdemo/JwtAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.indorm1992.jwtappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
